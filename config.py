42e77b4742e242da563c36884655f42a013b03ed
Wey Gu <littlewey@gmail.com>
2017-08-03 02:06:32 +0800

    optimized config, add json-fy state

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-phantomjsLogPath = "/var/log/phantomjs.log"
+phantomjsLogPath = r"phantomjs.log"
+userAgent        = r"Mozilla/5.0 (X11; CrOS x86_64 9460.73.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.134 Safari/537.36"
+url_initial      = r"http://www.puxiangdao.cn/resident/"
+url_status       = r"http://www.puxiangdao.cn/Resident/My_Home/HomeControl.aspx"
--- switch.py
+++ switch.py
@@ -5,46 +5,52 @@ import time
 from bs4 import BeautifulSoup
 from credential import *
 from config import *
+import json
+
 
 dcap = dict(DesiredCapabilities.PHANTOMJS)
-dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (X11; CrOS x86_64 9460.73.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.134 Safari/537.36")
+dcap["phantomjs.page.settings.userAgent"] = (userAgent)
 
 driver = webdriver.PhantomJS(desired_capabilities=dcap, service_log_path=phantomjsLogPath)
 
 driver.set_window_size(1024, 768)
-driver.get('http://www.puxiangdao.cn/Resident/')
-
-
+driver.get(url_initial)
+# login
 elementUser = driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_txtID"]')
 elementUser.send_keys(user)
-
 elementPSW = driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_txtPW"]')
 elementPSW.send_keys(password)
-
-
 elementPSW.send_keys(Keys.ENTER)
-
 time.sleep(1)
-
-driver.get('http://www.puxiangdao.cn/Resident/My_Home/HomeControl.aspx')
+# got status
+driver.get(url_status)
 
 statusHtml = driver.page_source
 soup = BeautifulSoup(statusHtml,"html.parser")
-
 
 light1=soup.find(id="ctl00_ContentPlaceHolder1_Repeater1_ctl01_label3").text
 light2=soup.find(id="ctl00_ContentPlaceHolder1_Repeater1_ctl02_label3").text
 light3=soup.find(id="ctl00_ContentPlaceHolder1_Repeater1_ctl03_label3").text
 light4=soup.find(id="ctl00_ContentPlaceHolder1_Repeater1_ctl04_label3").text
 
+allState = {
+    "1" : light1_Powerstate,
+    "2" : light2_Powerstate,
+    "3" : light3_Powerstate,
+    "4" : light4_Powerstate
+}
 
+allStateJson = json.dumps(allState)
+
+#print allStateJson
 
 elementLight1_0 = driver.find_element_by_xpath('//*[@id="ctl00_ContentPlaceHolder1_Repeater1_ctl03_tr"]')
 
 elementLight1_0.click()
 
 
-elementLight1_1 = driver.find_element_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_iBtn_Light_Off']")
+driver.save_screenshot('testing.png')
+elementLight1_1 = driver.find_element_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_iBtn_Light_On']")
 elementLight1_1.click()
 
 #driver.save_screenshot('testing.png')
